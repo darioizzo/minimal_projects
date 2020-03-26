f4bfa352f304a62e315a12166ba6b64d6be8d88d
Dario Izzo <dario.izzo@gmail.com>
2020-03-26 21:58:24 +0100

    revert

--- src/core.cpp
+++ src/core.cpp
@@ -3,35 +3,39 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-
 #include "naive_converters.hpp"
 #include "naive_udxs.hpp"
 #include "pickle_support.hpp"
 
 namespace py = pybind11;
 
-struct extracting_uda {
+struct extracting_uda
+{
   // Implementation of the objective function.
-  pagmo::population evolve(const pagmo::population &pop) const {
+  pagmo::population evolve(const pagmo::population &pop) const
+  {
     const auto &prob = pop.get_problem();
     auto upd_ptr = prob.extract<py::object>();
-    if (!upd_ptr) {
+    if (!upd_ptr)
+    {
       std::cout << "Cannot convert to py::object" << std::endl;
       throw 666;
     }
     return pop;
   }
   // Serialization
-  template <typename Archive> void serialize(Archive &ar, unsigned){};
+  template <typename Archive>
+  void serialize(Archive &ar, unsigned){};
 };
 
-bool is_pyobject(const pagmo::problem& p)
+bool is_pyobject(const pagmo::problem &p)
 {
   auto retval = p.get_type_index() == std::type_index(typeid(py::object));
   return retval;
 }
 
-PYBIND11_MODULE(core, m) {
+PYBIND11_MODULE(core, m)
+{
   // Converters tests
   m.def("test_population_converters", &test_population_converters);
   // Miscellanea
@@ -41,7 +45,7 @@ PYBIND11_MODULE(core, m) {
   extracting_uda_.def(py::init<>())
       .def("evolve", &extracting_uda::evolve)
       .def(py::pickle(&udx_pickle_getstate<extracting_uda>,
-                      &udx_pickle_setstate<extracting_uda>));         
+                      &udx_pickle_setstate<extracting_uda>));
   // Naive UDXs
   py::class_<naive_udp> naive_udp_(m, "naive_udp");
   naive_udp_.def(py::init<>())
