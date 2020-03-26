e2df7165bd9c432829118e3ed98228c3b96a6db1
Dario Izzo <dario.izzo@gmail.com>
2020-03-26 22:07:48 +0100

    minimal extended

--- src/core.cpp
+++ src/core.cpp
@@ -3,30 +3,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "naive_converters.hpp"
-#include "naive_udxs.hpp"
-#include "pickle_support.hpp"
-
 namespace py = pybind11;
-
-struct extracting_uda
-{
-  // Implementation of the objective function.
-  pagmo::population evolve(const pagmo::population &pop) const
-  {
-    const auto &prob = pop.get_problem();
-    auto upd_ptr = prob.extract<py::object>();
-    if (!upd_ptr)
-    {
-      std::cout << "Cannot convert to py::object" << std::endl;
-      throw 666;
-    }
-    return pop;
-  }
-  // Serialization
-  template <typename Archive>
-  void serialize(Archive &ar, unsigned){};
-};
 
 bool is_pyobject(const pagmo::problem &p)
 {
@@ -34,28 +11,21 @@ bool is_pyobject(const pagmo::problem &p)
   return retval;
 }
 
+bool can_extract_pyobject(const pagmo::problem &p)
+{
+  auto upd_ptr = p.extract<py::object>();
+  if (!upd_ptr)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
 PYBIND11_MODULE(core, m)
 {
-  // Converters tests
-  m.def("test_population_converters", &test_population_converters);
-  // Miscellanea
   m.def("is_pyobject", &is_pyobject);
-  // UDA to test for the extract from pythonic object
-  py::class_<extracting_uda> extracting_uda_(m, "extracting_uda");
-  extracting_uda_.def(py::init<>())
-      .def("evolve", &extracting_uda::evolve)
-      .def(py::pickle(&udx_pickle_getstate<extracting_uda>,
-                      &udx_pickle_setstate<extracting_uda>));
-  // Naive UDXs
-  py::class_<naive_udp> naive_udp_(m, "naive_udp");
-  naive_udp_.def(py::init<>())
-      .def("fitness", &naive_udp::fitness)
-      .def("get_bounds", &naive_udp::get_bounds)
-      .def(py::pickle(&udx_pickle_getstate<naive_udp>,
-                      &udx_pickle_setstate<naive_udp>));
-  py::class_<naive_uda> naive_uda_(m, "naive_uda");
-  naive_uda_.def(py::init<>())
-      .def("evolve", &naive_uda::evolve)
-      .def(py::pickle(&udx_pickle_getstate<naive_uda>,
-                      &udx_pickle_setstate<naive_uda>));
+  m.def("can_extract_pyobject", &can_extract_pyobject);
 }
---
+++ test/test_pyobject.py
@@ -0,0 +1,11 @@
+import pygmo
+import core
+class python_prob:
+    def fitness(self, dv):
+        return [dv[0]]
+    def get_bounds(self):
+        return ([0],[1])
+
+print("Is inner a py::object: ", core.is_pyobject( pygmo.problem(python_prob())))
+print("Can I extract a py::object from pagmo::problem: ", core.is_pyobject( pygmo.problem(python_prob())))
+
